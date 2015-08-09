2cbbb55d4074b4130c7d1b04a04703193a509f65
Matthew Danskey <mdanskey@gmail.com>
2015-08-08 22:44:24 -0700

    add class methods, message passing, proxy object project

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -15,10 +15,27 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class Proxy
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  attr_accessor :messages
+
+  def method_missing(name, *args, &block)
+    if @object.respond_to?(name)
+      @messages << name
+      @object.__send__(name, *args, &block)
+    else
+      super(name, *args, &block)
+    end
+  end
+
+  def called?(name)
+    !@messages.index(name).nil?
+  end
+
+  def number_of_times_called(name)
+    @messages.count name
+  end
 end
 
 # The proxy object should pass the following Koan:
